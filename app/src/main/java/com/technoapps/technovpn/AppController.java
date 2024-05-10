851512d7206d6f4fb12ee8dc3e91110d739e0c8d
amine <bouchmalaabesp@gmail.com>
2024-05-10 20:03:11 +0200

    second commit

---
+++ app/src/main/java/com/technoapps/technovpn/AppController.java
@@ -0,0 +1,74 @@
+package com.technoapps.technovpn;
+
+
+import static com.technoapps.technovpn.Constant.ONESIGNAL_APP_ID;
+
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.onesignal.OneSignal;
+
+public class AppController extends App {
+
+    public static final String TAG = AppController.class.getSimpleName();
+
+    private RequestQueue mRequestQueue;
+
+    private static AppController mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this);
+        OneSignal.setAppId(ONESIGNAL_APP_ID);
+
+        // promptForPushNotifications will show the native Android notification permission prompt.
+        // We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 7)
+        OneSignal.promptForPushNotifications();
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+    }
+
+    public static synchronized AppController getInstance() {
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+}
---
+++ app/src/main/java/com/technoapps/technovpn/service/BoostAlarm.java
@@ -0,0 +1,38 @@
+package com.technoapps.technovpn.service;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+import com.technoapps.technovpn.Fragments.PhoneBooster_Fragment;
+import com.technoapps.technovpn.R;
+
+public class BoostAlarm extends BroadcastReceiver {
+
+    public final static String PREFERENCES_RES_BOOSTER = "pharid";
+
+    SharedPreferences.Editor editor;
+    SharedPreferences sharedpreferences;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        sharedpreferences = context.getSharedPreferences(PREFERENCES_RES_BOOSTER, Context.MODE_PRIVATE);
+
+        editor = sharedpreferences.edit();
+        editor.putString("booster", "1");
+        editor.commit();
+
+        try {
+            PhoneBooster_Fragment.optimizebutton.setBackgroundResource(0);
+            PhoneBooster_Fragment.optimizebutton.setImageResource(0);
+            PhoneBooster_Fragment.optimizebutton.setImageResource(R.drawable.clear_btn);
+        }
+        catch(Exception e)
+        {
+
+        }
+
+    }
+}
